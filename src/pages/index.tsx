7476b9d7f1e687b35d12c74a289d3a91731f95ff
William, Garcia <williamvinicios_838@hotmail.com>
2022-12-08 20:24:50 -0300

    Sorteio funcionando

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,24 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { Input } from "../components/Input";
-import { ManagamentNamesContext, ManagamentNamesProvider } from "../context/ManagamentNamesContext";
-import styles from "../styles/pages/Home.module.css"
-
-
-
+import { ManagamentNamesContext } from "../context/ManagamentNamesContext";
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
-  
-  
-  const {list, issorted, sorteado, sortear}=useContext(ManagamentNamesContext)
-  console.log(list, issorted, sorteado)
-  
-  return (
-    <ManagamentNamesProvider>
+  const { list, issorted, sorteado, sortear } = useContext(
+    ManagamentNamesContext
+  );
 
+  return (
     <div className={styles.homecontainer}>
       <div className={styles.listcontainer}>
-        <Input/>  
+        <Input />
       </div>
 
       <div className={styles.sorteiocontainer}>
-        <img src="/coquinha.png"/>
-                {
-                  issorted ? <h2>{list[sorteado]}</h2> : <h2>Sortudo</h2>
-                }
+        <img src="/coquinha.png" />
+        {issorted ? <h2>{list[sorteado]}</h2> : <h2>Sortudo</h2>}
         <button onClick={sortear}>Sortear</button>
       </div>
     </div>
-    
-    </ManagamentNamesProvider>
-
-  )
+  );
 }
-
-
